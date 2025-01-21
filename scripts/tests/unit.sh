a3006f5bb9912a139433acb2a0ca2cb92e9e5d46
Chris Walters <chris.walters@nhs.net>
2025-01-21 19:56:00 +0000

    calling unit tests

--- scripts/tests/unit.sh
+++ scripts/tests/unit.sh
@@ -17,4 +17,6 @@ cd "$(git rev-parse --show-toplevel)"
 # tests from here. If you want to run other test suites, see the predefined
 # tasks in scripts/test.mk.
 
-echo "Unit tests are not yet implemented. See scripts/tests/unit.sh for more."
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+python -m unittest discover
